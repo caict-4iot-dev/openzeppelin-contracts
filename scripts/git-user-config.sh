d0825f89d8d39d003c4dba936739517610679da9
maxufeng <maxufeng@caict.ac.cn>
2024-03-13 11:29:31 +0800

    [doc] update pack file

--- scripts/git-user-config.sh
+++ scripts/git-user-config.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail -x
 
-git config user.name 'github-actions'
-git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
+git config user.name 'maxufeng'
+git config user.email 'maxufeng_sx@163.com'
